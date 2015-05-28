0f94039242237aabef06ed89e259931d15be752a
WangTeng <woaijia_xxx@126.com>
2015-05-28 14:55:01 +0800

    init
    init

---
+++ UIStrikethroughLabel/source/UIStrikethroughLabel.swift
@@ -0,0 +1,21 @@
+//
+//  UIStrikethroughLabel.swift
+//  UIStrikethroughLabel
+//
+//  Created by Tiny on 15/5/28.
+//  Copyright (c) 2015年 Murphy. All rights reserved.
+//
+
+import UIKit
+
+class UIStrikethroughLabel: UILabel {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
